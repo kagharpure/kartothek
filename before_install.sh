b8f130e5a2a3a94e8cd392e1f3db22735d2ef078
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-16 20:41:06 +0530

    Fix for OSX: check env var

--- before_install.sh
+++ before_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-#    #brew update
-#    #brew install pyenv
-#    #pyenv install
-#    #source venv/bin/activate
-#    for p in $(which -a python); do
-#        eval $($p --version)
-#    done
+#if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+#
+#
+#
+#fi
+
+pip install --upgrade pip
+pip install pip-tools
+./compile_requirements.sh
+python setup.py bdist_wheel
+if [ "$TRAVIS_PYTHON_VERSION" = $PY36V* ]; then
+    echo "36 here"
 fi
