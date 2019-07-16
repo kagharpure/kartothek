d0dc32ca7d5f714c7c9fb1472f5dce1c68bb7ca4
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-16 20:49:08 +0530

    Fix for OSX: check env var 2

--- before_install.sh
+++ before_install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-#
-#
-#
-#fi
+if [ ${TRAVIS_PYTHON_VERSION:0:3} = $PY36V ]; then
+    export PY36V
+fi
+
+if [ $TRAVIS_OS_NAME = "osx" ]; then
+    echo "Got OSX"
+    echo $PY36V
+
+fi
 
 pip install --upgrade pip
 pip install pip-tools
 ./compile_requirements.sh
 python setup.py bdist_wheel
-if [ "$TRAVIS_PYTHON_VERSION" = $PY36V* ]; then
-    echo "36 here"
-fi
