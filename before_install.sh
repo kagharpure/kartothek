708fe4238dadf739f67edd2a1a4274aa13bb48b4
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-16 20:53:09 +0530

    Fix for OSX: check env var 4

--- before_install.sh
+++ before_install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ ${TRAVIS_PYTHON_VERSION:0:3} = $PY36V ]; then
-    export PY36V
+if [ ${TRAVIS_PYTHON_VERSION:0:3} = 3.6 ]; then
+    export PY36V=$TRAVIS_PYTHON_VERSION
+elif [ ${TRAVIS_PYTHON_VERSION:0:3} = 3.7 ]; then
+    export PY37V=$TRAVIS_PYTHON_VERSION
 fi
 
 if [ $TRAVIS_OS_NAME = "osx" ]; then
     echo "Got OSX"
     echo $PY36V
+    echo $PY37V
 
 fi
 
