19e9729ec2f00e3e3c0741e9ac5b4c3de2f19b2d
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-16 16:48:16 +0530

    Fix for OSX: conditionally back to brew

--- before_install.sh
+++ before_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    #brew update
-    #brew install pyenv
-    #pyenv install
-    #source venv/bin/activate
-    for p in $(which -a python); do
-        eval $($p --version)
-    done
+#    #brew update
+#    #brew install pyenv
+#    #pyenv install
+#    #source venv/bin/activate
+#    for p in $(which -a python); do
+#        eval $($p --version)
+#    done
 fi
