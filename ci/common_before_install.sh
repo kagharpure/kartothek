90607dd55ed23c041f0b18da0c7e5ff0bdde3417
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-22 10:26:51 +0200

    Move scripts to ci folder

--- ci/common_before_install.sh
+++ ci/common_before_install.sh
@@ -3,5 +3,5 @@ set -xeo pipefail
 
 pip install --upgrade pip
 pip install pip-tools
-./compile_requirements.sh
+./ci/compile_requirements.sh
 python setup.py bdist_wheel
---
+++ ci/osx_before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -xeo pipefail
+
+pip3 install virtualenv
+virtualenv -p python3 ~/kartothek_osx_test_env
+source ~/kartothek_osx_test_env/bin/activate
+./ci/common_before_install.sh
