b4e067dd93da247fc591180104ff6173b5af7e53
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-22 10:08:56 +0200

    Test run

--- before_install.sh
+++ before_install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-
-if [ "$TRAVIS_PYTHON_VERSION" = 3.6 ]; then
-    echo "$TRAVIS_PYTHON_VERSION" > ./python_versions/pyv3.6
-    cat ./python_versions/pyv3.6
-elif [ "$TRAVIS_PYTHON_VERSION" = 3.7 ]; then
-    echo "$TRAVIS_PYTHON_VERSION" > ./python_versions/pyv3.7
-    cat ./python_versions/pyv3.7
-fi
-
-if [ $TRAVIS_OS_NAME = "osx" ]; then
-    echo "Got OSX"
-    cat ./python_versions/pyv3.6
-    cat ./python_versions/pyv3.7
-fi
+set -xeo pipefail
 
 pip install --upgrade pip
 pip install pip-tools
