20bc784c243a34a9f42cc79ae10c7bd817d57696
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-22 13:45:09 +0200

    No virtualenv for osx

--- ci/osx_before_install.sh
+++ ci/osx_before_install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -xeo pipefail
 
-python3 --version
-pip3 install virtualenv
-virtualenv -p python3 ~/kartothek_osx_test_env
-source ~/kartothek_osx_test_env/bin/activate
-which python
-python --version
+#pip3 install virtualenv
+#virtualenv -p python3 ~/kartothek_osx_test_env
+#source ~/kartothek_osx_test_env/bin/activate
 ./ci/common_before_install.sh
