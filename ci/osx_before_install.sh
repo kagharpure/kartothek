51de1dabbe5e0da53a77af90cdb2a4ea2b5e454e
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-22 11:15:31 +0200

    Check python version in virtual environment

--- ci/osx_before_install.sh
+++ ci/osx_before_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -xeo pipefail
 
+python3 --version
 pip3 install virtualenv
 virtualenv -p python3 ~/kartothek_osx_test_env
 source ~/kartothek_osx_test_env/bin/activate
+which python
+python --version
 ./ci/common_before_install.sh
