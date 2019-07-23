7afc5759ab93cd27cc175a450156e6b31d646927
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-23 17:31:43 +0200

    Try all before install together in one script for osx

--- ci/osx_before_install.sh
+++ ci/osx_before_install.sh
@@ -6,4 +6,13 @@ pip install virtualenv
 virtualenv -p python3 ~/kartothek_osx_test_env
 source ~/kartothek_osx_test_env/bin/activate
 which pip
+pip install --upgrade pip!=19.2
+pip install pip-tools
+which pip
+which pip-compile
+./ci/compile_requirements.sh
+which python
+python setup.py bdist_wheel
+
+
 #pyenv install --list
--- ci/upgrade_pip_install_pip_tools.sh
+++ ci/upgrade_pip_install_pip_tools.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -xeo pipefail
 
-which pip
-pip install --upgrade pip!=19.2
-pip install pip-tools
-which pip
