a3bf33cf2b68611d0fd88110c2391d3a88d7eaf3
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-23 16:08:07 +0200

    Try osx with venv again

--- ci/osx_before_install.sh
+++ ci/osx_before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xeo pipefail
 
-#pip3 install virtualenv
-#virtualenv -p python3 ~/kartothek_osx_test_env
-#source ~/kartothek_osx_test_env/bin/activate
-pyenv install --list
+pip3 install virtualenv
+virtualenv -p python3 ~/kartothek_osx_test_env
+source ~/kartothek_osx_test_env/bin/activate
+#pyenv install --list
 ./ci/common_before_install.sh
