afb51aab576df41748ffd6ac1286fe67b5abd597
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-24 11:29:18 +0200

    Try something different

--- ci/osx_before_install.sh
+++ ci/osx_before_install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -xeo pipefail
 
-#which pip
-#pip install virtualenv
-#virtualenv -p python3 ~/kartothek_osx_test_env
-#source ~/kartothek_osx_test_env/bin/activate
-#which pip
-pip3 install --upgrade pip==19.1.*
-pip3 install pip-tools
-./ci/compile_requirements.sh
-python3 setup.py bdist_wheel
-
-
-#pyenv install --list
+python3 -m pip install --upgrade virtualenv
+virtualenv -p python3 --system-site-packages "$HOME/venv"
+source "$HOME/venv/bin/activate"
