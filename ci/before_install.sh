1aedfa0c7c8d0b0199282d6e93c838f4197490e4
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-26 12:31:30 +0200

    Cleanup

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -6,16 +6,10 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     python3 -m pip install --upgrade virtualenv
     virtualenv -p python3 "$HOME/ktk_venv"
     source "$HOME/ktk_venv/bin/activate"
-    which python
-    which pip
 fi
 
 # https://github.com/JDASoftwareGroup/kartothek/issues/94
-which pip
 pip install --upgrade pip==19.1.*
-which pip
 pip install pip-tools
-which pip-compile
-source compile_requirements.sh
-which python
+./ci/compile_requirements.sh
 python setup.py bdist_wheel
