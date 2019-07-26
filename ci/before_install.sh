8ced5dfa4520355a1cf5cbbb3e8e42c5fd674fdc
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-26 13:50:27 +0200

    Move before_install to script

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -xeo pipefail
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    [ ! -z "$(which python3)" ] && \
-    [[ $(python3 --version | cut -d ' ' -f2) != 3.* ]] && \
-    exit 1
-    python3 -m pip install --upgrade virtualenv
-    virtualenv -p python3 "$HOME/ktk_venv"
-    source "$HOME/ktk_venv/bin/activate"
-fi
-
 # https://github.com/JDASoftwareGroup/kartothek/issues/94
 pip install --upgrade pip==19.1.*
 pip install pip-tools
