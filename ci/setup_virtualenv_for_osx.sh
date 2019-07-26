924622e8ad7fb5136cef89d89a1490d1955f2314
Krishnaraj Gharpure <krishnaraj.gharpure@jda.com>
2019-07-26 13:58:26 +0200

    For travis

--- ci/setup_virtualenv_for_osx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xeo pipefail
-
-[ ! -z "$(which python3)" ] && \
-[[ $(python3 --version | cut -d ' ' -f2) != 3.* ]] && \
-exit 1
-python3 -m pip install --upgrade virtualenv
-virtualenv -p python3 "$HOME/ktk_venv"
-source "$HOME/ktk_venv/bin/activate"
